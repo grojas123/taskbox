ac1307c366db114308c5ead86eb4ce4eb8a9919c
Gustavo Rojas <grojas@fastmail.com>
2023-01-12 14:25:20 -0600

    Construct a screen out of components

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,11 @@
 import '../src/index.css';
 // Configures Storybook to log the actions (onArchiveTask and onPingTask) in the UI.
+//Register the msw addon
+import {initialize,mswDecorator} from "msw-storybook-addon";
+//Initialize MWS
+initialize();
+//Provide the MSW addon decorator globally
+export const decorators=[mswDecorator];
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
--- src/lib/store.js
+++ src/lib/store.js
@@ -1,10 +1,13 @@
 /* A simple redux store/actions/reducer implementation.
 * A true app would be more complex and separated into different files.
  */
-import {configureStore,createSlice} from "@reduxjs/toolkit";
+import {
+    configureStore,
+    createSlice,
+    createAsyncThunk,} from "@reduxjs/toolkit";
 /*
 * The initial state of our store when the app loads.
-* Usually, you would fetch this from a server. Let's no worry about that now
+* Usually, you would fetch this from a server. Lets no worry about that now
  */
 const defaultTasks=[
     {id:'1', title:'Something',state:'TASK_INBOX'},
@@ -17,6 +20,26 @@ const TaskBoxData={
     status:'idle',
     error:null,
 }
+/*
+* Created and asyncThunk to fetch tasks from a remote endpoint.
+* You can read more about Redux Toolkit's thunks in the docs:
+* https://redux-tookit.js.org/api/createAsyncThunk
+ */
+export const fetchTasks = createAsyncThunk('todos/fetchTodos', async ()=>{
+    const response=await fetch(
+        'https://jsonplaceholder.typicode.com/todos?userId=1'
+    );
+    const data = await response.json();
+    const result = data.map((task)=>({
+        id:`${task.id}`,
+        title:task.title,
+        state:task.completed?'TASK_ARCHIVED' :'TASK_INBOX'
+
+    }));
+    return result;
+    });
+
+
 /*
 *The store is created here.
 * You can read more about Redux Toolkit's slices in the docs:
@@ -34,6 +57,25 @@ const TasksSlice= createSlice({
             }
         },
     },
+    extraReducers(builder){
+        builder
+            .addCase(fetchTasks.pending,(state)=>{
+                state.status='loading';
+                state.error=null;
+                state.tasks=[];
+            })
+            .addCase(fetchTasks.fulfilled,(state,action) =>{
+                state.status='succeded';
+                state.error=null;
+                // Add any fetched tasks to the array
+                state.tasks=action.payload;
+            })
+            .addCase(fetchTasks.rejected,(state)=>{
+                state.status='failed';
+                state.error="Something went wrong";
+                state.tasks = [];
+            })
+    }
 });
 //The actions contained in the slice are exported for usage in our components
 export const {updateTaskState}=TasksSlice.actions;
